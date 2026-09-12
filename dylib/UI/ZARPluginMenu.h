19c3768317b4248dfd313490f4e3223fc81216ed
FadedRK <105328746+FadedRK@users.noreply.github.com>
2026-09-12 15:20:45 +0700

    feat: add ZolaAntiRecall plugin menu interface

---
+++ dylib/UI/ZARPluginMenu.h
@@ -0,0 +1,12 @@
+#import <UIKit/UIKit.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void ZARInstallPluginMenuHook(void);
+void ZARInstallPluginMenuIfNeeded(void);
+
+#ifdef __cplusplus
+}
+#endif
