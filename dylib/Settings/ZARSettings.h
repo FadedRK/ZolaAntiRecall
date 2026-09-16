6206a5da4a0560ea88367177e44d94b5f5d59d14
FadedRK <105328746+FadedRK@users.noreply.github.com>
2026-09-16 13:56:55 +0700

    feat: expose my recall display setting

--- dylib/Settings/ZARSettings.h
+++ dylib/Settings/ZARSettings.h
@@ -1,5 +1,14 @@
 #import <UIKit/UIKit.h>
 
+@interface ZARSettings : NSObject
+
++ (instancetype)sharedInstance;
+
+/// 是否显示自己撤回的消息。YES = 拦截自己撤回并显示原文；NO = 正常撤回。
+@property (nonatomic, assign) BOOL showMyRecallEnabled;
+
+@end
+
 #ifdef __cplusplus
 extern "C" {
 #endif
